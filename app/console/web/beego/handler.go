5e583bfb2157d476c9f0d0930dd86b874732f3f8
Hobo86 <hobo@hbchen.com>
2019-12-13 13:05:53 +0800

    console api micro pb文件 console web 格式化

--- app/console/web/beego/handler.go
+++ app/console/web/beego/handler.go
@@ -1,21 +1,21 @@
 package beego
 
 import (
-	"context"
-	"github.com/micro/go-micro/util/log"
-	bctx "github.com/astaxie/beego/context"
-	"github.com/astaxie/beego"
-)
+	"net/http"
 
+	"github.com/astaxie/beego"
+	bctx "github.com/astaxie/beego/context"
+	"github.com/micro/go-micro/util/log"
+)
 
 //New return a beego handler
 func New() (http.Handler, error) {
 
-	beego.Get("/demo", func (ctx *bctx.Context) {
+	beego.Get("/demo", func(ctx *bctx.Context) {
 		log.Info("Received Get Request")
 		ctx.Output.JSON(map[string]string{
 			"message": "BeeGo Here",
 		}, false, true)
 	})
-	return 	beego.BeeApp.Handlers, nil
+	return beego.BeeApp.Handlers, nil
 }
