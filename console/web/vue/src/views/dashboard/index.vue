b11b60b6b8ced07f8c276b67b2b430b98e159c66
hbchen <hobo@hbchen.com>
2020-03-15 12:42:36 +0800

    console dashboard add demo list

--- console/web/vue/src/views/dashboard/index.vue
+++ console/web/vue/src/views/dashboard/index.vue
@@ -1,11 +1,31 @@
 <template>
   <div class="dashboard-container">
     <div class="dashboard-text">name: {{ name }}</div>
+    <el-table
+      v-loading="listLoading"
+      :data="list"
+      element-loading-text="Loading"
+      border
+      fit
+      highlight-current-row
+    >
+      <el-table-column align="center" label="页面" width="150">
+        <template slot-scope="scope">
+          {{ scope.row.name }}
+        </template>
+      </el-table-column>
+      <el-table-column label="链接">
+        <template slot-scope="scope">
+          <a @click.prevent="handleLink(scope.row)">{{ scope.row.path }}</a>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import pathToRegexp from "path-to-regexp";
 
 export default {
   name: 'Dashboard',
@@ -13,6 +33,46 @@ export default {
     ...mapGetters([
       'name'
     ])
+  },
+  data() {
+    return {
+      list: [
+        {
+          'name': 'Gateway',
+          'path': '/',
+        },
+        {
+          'name': 'Gateway Metrics',
+          'path': '/metrics',
+        },
+        {
+          'name': 'Console首页',
+          'path': '/console/',
+        },
+        {
+          'name': 'Echo',
+          'path': '/console/v1/echo/',
+        },
+        {
+          'name': 'Gin',
+          'path': '/console/v1/gin/',
+        },
+        {
+          'name': 'Iris',
+          'path': '/console/v1/iris/',
+        },
+        {
+          'name': 'Beego',
+          'path': '/console/v1/beego/',
+        },
+      ],
+      listLoading: false
+    }
+  },
+  methods: {
+    handleLink(page) {
+      window.open(page.path, '_blank');
+    }
   }
 }
 </script>
