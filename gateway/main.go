2cc42571acb9eb9936b278b0c1836272e2e0deab
陈洪波 <chenhongbo@szkingdom.com>
2020-11-19 00:16:25 +0800

    使用config加载配置

--- gateway/main.go
+++ gateway/main.go
@@ -3,8 +3,14 @@ package main
 import (
 	"github.com/stack-labs/stack-rpc"
 	"github.com/stack-labs/stack-rpc-plugins/service/gateway"
+	"github.com/stack-labs/stack-rpc/plugin"
+	"github.com/stack-labs/stack-rpc/server/mock"
 	"github.com/stack-labs/stack-rpc/util/log"
 )
+
+func init() {
+	plugin.DefaultServers["mock"] = mock.NewServer
+}
 
 func main() {
 	svc := stack.NewService()
